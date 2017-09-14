681991c62b3ae2e005dfe74c58f97f069ff7d7ff
NE NightElves <nenightelves@hotmail.com>
2017-09-14 13:03:03 +0800

    List

--- SeqList.java
+++ SeqList.java
@@ -1,28 +1,3 @@
-class Node<T>
-{
-
-    public T data;
-    public Node<T> next;
-
-    public Node(T data, Node<T> next)
-    {
-        this.data = data;
-        this.next = next;
-    }
-
-    public Node()
-    {
-
-        this(null, null);
-    }
-
-    public String toString()
-    {
-
-        return this.data.toString();
-    }
-}
-
 public class Seqlist<T> extends Object
 {
 
